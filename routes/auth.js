71d7fba7e1f5834a54cfcd6f379194791ed942aa
GeoffreyCoding <Geoffrey.huth@gmail.com>
2023-11-07 10:05:42 +0200

    commenting

--- routes/auth.js
+++ routes/auth.js
@@ -6,17 +6,19 @@ const ExpressBrute = require('express-brute')
 const store = new ExpressBrute.MemoryStore()
 const bruteforce = new ExpressBrute(store)
 
-//login route
+//login route with brute force prevention
 router.post('/',bruteforce.prevent,async (req,res) =>{
+    //gets a single user
     const user = await User.findOne({username: req.body.username});
+    //no user found
     if(!user)
+    //wrong username/password
         return res.status(401).json({error: 'Incorrect username or password.'});
-    
     const valid = await isValidPassword(req.body.password,user.password);
 
     if(!valid)
         return res.status(401).json({error: 'Incorrect username or password.'});
-
+    //successful login signing auth token
     const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET_KEY)
     res.send({token});
 });
